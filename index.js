c63d001215a36d2f5b19b0878e3a7c31f75d9125
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2025-10-31 10:23:33 +0100

    making the data displayed in the event and on the tycket dynami and restructuring js variables

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const forward = document.querySelector(".plus-step");
-const backward = document.querySelector(".minus-step");
-const sectionSlider = document.querySelector(".sectionSlider");
-const stepper = document.querySelectorAll(".stepper");
-const mobileStepper = document.querySelector(".stepper-mobile");
-const chooseEventButton = document.getElementById("choose-event-button");
-const evantCards = document.querySelectorAll("[data-eventId]");
-const ticketCounter = document.querySelector(".ticketCounter");
-const previousButtons = document.querySelectorAll(".previous-b")
-const nextButtons = document.querySelectorAll(".next-b")
+const eventListContainer = document.querySelector(".eventListOne");
+const eventListContainer2 = document.querySelector(".eventListTwo");
 
 let step = 0;
 let eventId = null;
@@ -80,22 +72,80 @@ let events = [
   },
 ];
 
+/** creating the list of events  in html using this array */
+events.map((event, i) => {
+  let li = document.createElement("li");
+  li.setAttribute("data-eventId", event.id);
+  li.classList.add(
+    "flex",
+    "flex-col",
+    "gap-8",
+    "p-5",
+    "rounded-2xl",
+    "justify-between",
+    "bg-primary"
+  );
+  let content = `
+      <div class="flex flex-col gap-2">
+        <p class="font-semibold text-2xl">${event.name}</p>
+        <div class="opacity-85 font-medium flex items-center">
+          <p>${event.places}</p>
+          <img
+            src="assets/images/stool.png"
+            alt="chair"
+            class="size-4"
+          />
+        </div>
+      </div>
+
+      <p class="font-semibold text-md">${event.desc}</p>
+
+      <div class="flex items-center justify-between">
+        <div>
+          <p class="font-semibold">
+            15:00 - 17:00
+            <span class="text-lg opacity-85">${event.date}</span>
+          </p>
+          <p class="font-semibold">${event.location}</p>
+        </div>
+        <img src="assets/images/right-arrow.png" class="size-5" />
+      </div>
+    `;
+  li.innerHTML = content;
+  if (i <= 3) {
+    eventListContainer.appendChild(li);
+    return;
+  }
+  eventListContainer2.appendChild(li);
+});
+
+const forward = document.querySelector(".plus-step");
+const backward = document.querySelector(".minus-step");
+const sectionSlider = document.querySelector(".sectionSlider");
+const stepper = document.querySelectorAll(".stepper");
+const mobileStepper = document.querySelector(".stepper-mobile");
+const chooseEventButton = document.getElementById("choose-event-button");
+const evantCards = document.querySelectorAll("[data-eventId]");
+const ticketCounter = document.querySelector(".ticketCounter");
+const previousButtons = document.querySelectorAll(".previous-b");
+const nextButtons = document.querySelectorAll(".next-b");
+const plus = document.querySelector(".counter-plus");
+const minus = document.querySelector(".counter-minus");
+
 // initialization
 buttonInablerAndDisabler(chooseEventButton, eventId !== null ? false : true);
 buttonInablerAndDisabler(nextButtons[0], ticketsBooked === 0 ? true : false);
 initializeAndUpdateProgress();
 
-
-
-
 // event listenner to listen for the clicking of choosin an event
 evantCards.forEach((event) => {
   event.addEventListener("click", (e) => {
-      ticketsBooked = 0;
-      ticketCounter.textContent = ticketsBooked;
+    ticketsBooked = 0;
+    ticketCounter.textContent = ticketsBooked;
     if (Number(event.getAttribute("data-eventId")) === eventId) {
       event.classList.remove("selected-event");
       eventId = null;
+      ticketsDetailFiller();
 
       buttonInablerAndDisabler(chooseEventButton, true);
       return;
@@ -107,10 +157,9 @@ evantCards.forEach((event) => {
     eventId = Number(e.currentTarget.getAttribute("data-eventId"));
     buttonInablerAndDisabler(chooseEventButton, step !== null ? false : true);
     e.currentTarget.classList.toggle("selected-event");
+    ticketsDetailFiller();
   });
 });
-
-
 
 // event listenner to listen for the click of the choose event button to go to the next step
 chooseEventButton.addEventListener("click", (e) => {
@@ -123,18 +172,47 @@ chooseEventButton.addEventListener("click", (e) => {
   console.log(eventId);
 });
 
-let plus = document.querySelector(".counter-plus");
-let minus = document.querySelector(".counter-minus");
+// addint the event details to the ticket one an event is shosen
 
+const ticketsDetailFiller = () => {
+  const ticketDetailContainer = document.querySelector(
+    "[data-role='ticket-info-container']"
+  );
+
+  ticketDetailContainer.innerHTML = "";
+
+  if (eventId === null) return;
+
+  let content = `
+      <p class="text-center font-semibold text-3xl md:hidden">${events[eventId].name}</p>
+      <div class="flex items-center justify-between">
+        <p class="date-ticket font-semibold sm:text-lg whitespace-nowrap">${events[eventId].date}</p>
+        <p class="ticket-title max-md:hidden text-4xl font-semibold first-letter:capitalize text-center">${events[eventId].name}</p>
+        <div class="flex max-md:w-full items-center justify-center gap-3 md:gap-6 font-medium">
+          <p class="max-md:max-md:ml-auto">15:00 - 17:00</p>
+          <p class="location-ticket max-md:ml-auto">${events[eventId].location}</p>
+        </div>
+      </div>
+      <p class="font-medium max-sm:text-sm text-center text-white/75">Discover why your div still shifts 3px left for no reason. Bring tissues.</p>
+      <div class="flex items-baseline justify-center">
+        <img src="assets/images/ticketly-logo-ticket.png" class="md:ml-auto w-38 max-md:w-30" alt="logo">
+        <p class="font-bold ml-auto max-md:hidden">00007541289</p>
+      </div>
+  `;
+
+  ticketDetailContainer.innerHTML = content;
+  console.log(ticketDetailContainer);
+};
+
+// traking the plus button that increase the tickets
 plus.addEventListener("click", () => {
-  if (ticketsBooked === events[eventId - 1].places) return;
+  if (ticketsBooked === events[eventId].places) return;
   ticketsBooked++;
   ticketCounter.textContent = ticketsBooked;
   buttonInablerAndDisabler(nextButtons[0], ticketsBooked === 0 ? true : false);
 });
 
-
-
+// traking the minus button that decrease the tickets
 minus.addEventListener("click", () => {
   if (ticketsBooked === 0) return;
   ticketsBooked--;
@@ -142,43 +220,24 @@ minus.addEventListener("click", () => {
   buttonInablerAndDisabler(nextButtons[0], ticketsBooked === 0 ? true : false);
 });
 
-
-
-
-
 // previous button in second section
-previousButtons.forEach(button => {
-  button.addEventListener("click", () =>{
-    if(step == 1){
-      step--
-      initializeAndUpdateProgress()
+previousButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (step == 1) {
+      step--;
+      initializeAndUpdateProgress();
     }
-  })
-})
+  });
+});
 
-nextButtons.forEach(button => {
-  button.addEventListener("click", () =>{
-    if(step == 1){
-      if(ticketsBooked === 0) return
+nextButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (step == 1) {
+      if (ticketsBooked === 0) return;
     }
-    console.log('next')
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log("next");
+  });
+});
 
 // functions
 
@@ -216,26 +275,10 @@ function initializeAndUpdateProgress() {
   sectionSlider.style.transform = `translateX(${-100 * step}%)`;
 }
 
-
-
-
 const initializeTicketCounter = (value) => {
-  ticketCounter = value
+  ticketCounter = value;
   ticketCounter.textContent = value;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 // testing evantCards to be removed
 
